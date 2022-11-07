82c3aa655db673ebfdd49d7a49725b0761e95dd4
Maria Fernanda Perez Rojo <mariafperez603@gmail.com>
2022-11-07 00:56:39 -0500

    switching to query params

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,7 @@
 
 from app import db
 from app.models.book import Book
-from flask import Blueprint, jsonify, make_response, request
+from flask import Blueprint, jsonify, make_response, request, abort 
 
 # class Book:
 #     def __init__(self, id, title, description):
@@ -17,8 +17,22 @@ from flask import Blueprint, jsonify, make_response, request
 
 books_bp = Blueprint("books", __name__, url_prefix="/books")
 
+# helper functions
+def validate_book(book_id):
+    try:
+       book_id = int(book_id)
+    except:
+       abort(make_response({"message":f"book {book_id} invalid"}, 400))
+
+    book = Book.query.get(book_id)
+    if not book:
+        abort(make_response({"message":f"book {book_id} not found"}, 404))
+    
+    return book
+
+# route functions
 @books_bp.route("", methods=["POST"])
-def handle_books():
+def create_book():
     request_body = request.get_json()
     new_book = Book(title=request_body["title"],
                     description=request_body["description"])
@@ -28,39 +42,82 @@ def handle_books():
 
     return make_response(f"Book {new_book.title} successfully created", 201)
 
-#def validate_book(book_id):
-#    try:
-#        book_id = int(book_id)
-#    except:
-#        abort(make_response({"message":f"book {book_id} invalid"}, 400))
-#
-#    for book in books:
-#        if book.id == book_id:
-#            return book
-#
-#    abort(make_response({"message":f"book {book_id} not found"}, 404))
+@books_bp.route("", methods=["GET"])
+def read_all_books():
+    books_response = []
+    books = Book.query.all()
+    for book in books:
+        books_response.append(
+            {
+                "id": book.id,
+                "title": book.title,
+                "description": book.description
+            }
+        )
+    return jsonify(books_response)
+
+@books_bp.route("/<book_id>", methods=["GET"])
+def read_one_book(book_id):
+    book = validate_book(book_id)
+    return {
+          "id": book.id,
+          "title": book.title,
+          "description": book.description,
+    }
+
+@books_bp.route("/<book_id>", methods=["PUT"])
+def update_book(book_id):
+    book = validate_book(book_id)
+
+    request_body = request.get_json()
+
+    book.title = request_body["title"]
+    book.description = request_body["description"]
+ 
+    db.session.commit()
+   
+    return make_response(f"Book #{book.id} successfully updated")
+
+@books_bp.route("/<book_id>", methods=["DELETE"])
+def delete_book(book_id):
+    book = validate_book(book_id)
+
+    db.session.delete(book)
+    db.session.commit()
+
+    return make_response(f"Book #{book.id} successfully deleted")
+
+# @books_bp.route("", methods=["POST", "GET"])
+# def handle_books():
+#     if request.method == "POST":
+#         request_body = request.get_json()
+#         if "title" not in request_body or "description" not in request_body:
+#             return make_response("Invaild Request", 400)
+#         new_book = Book(title=request_body["title"],
+#                         description=request_body["description"])
+
+#         db.session.add(new_book)
+#         db.session.commit()
+
+#         return make_response(f"Book {new_book.title} successfully created", 201)
+#     elif request.method == "GET":
+
+# # @books_bp.route("", methods=["GET"])
+# # def handle_books():
+#         books = Book.query.all()
+#         books_response = []
+#         for book in books:
+#             books_response.append(
+#                 {
+#                     "id": book.id,
+#                     "title": book.title,
+#                     "description": book.description
+#                 }
+#             )
+#         return jsonify(books_response)
+
+
+
         
 
-# @books_bp.route("", methods=["GET"])
-# def handle_books():
-#     books_response = []
-#     for book in books:
-#         books_response.append(
-#             {
-#                 "id": book.id,
-#                 "title": book.title,
-#                 "description": book.description
-#             }
-#         )
-#     return jsonify(books_response)
-
-# @books_bp.route("/<book_id>", methods=["GET"])
-# def handle_book(book_id):
-#     book = validate_book(book_id)
-#
-#     return {
-#           "id": book.id,
-#           "title": book.title,
-#           "description": book.description,
-#     }
 
